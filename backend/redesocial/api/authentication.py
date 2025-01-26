cd153f2949db1aecf61222cd373365737b22b267
Augusto Reis <ask5.gnr@gmail.com>
2025-01-26 11:40:21 -0400

    some changes

--- backend/redesocial/api/authentication.py
+++ backend/redesocial/api/authentication.py
@@ -3,16 +3,18 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CookiesJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        acess_token = request.COOKIES.get("access_token")
+        auth_header = request.META.get("HTTP_AUTHORIZATION")
 
-        if not acess_token:
+        if not auth_header or not auth_header.startswith("Bearer "):
             return None
 
-        validated_token = self.get_validated_token(acess_token)
+        access_token = auth_header.split(" ")[1]
+
+        validated_token = self.get_validated_token(access_token)
 
         try:
             user = self.get_user(validated_token)
-        except:
+        except Exception as e:
             return None
 
         return (user, validated_token)
--- backend/redesocial/api/serializers.py
+++ backend/redesocial/api/serializers.py
@@ -1,5 +1,29 @@
 from .models import Users, Note
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+from django.contrib.auth.models import User, AbstractUser
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "password",
+        ]
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data["username"],
+            email=validated_data["email"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
 
 
 class UsersSerializer(serializers.ModelSerializer):
