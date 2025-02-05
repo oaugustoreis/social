a536cc5c974e3b490b6a51a82743afc877199817
Augusto Reis <ask5.gnr@gmail.com>
2025-02-04 21:31:38 -0400

    update models

--- backend/redesocial/api/models.py
+++ backend/redesocial/api/models.py
@@ -25,6 +25,7 @@ class Note(models.Model):
     description = models.CharField(max_length=200)
     data = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.description
